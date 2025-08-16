e4bb888d7aeefd7947d85704bd7e99fc15499f9d
lylewk <lylewk@users.noreply.github.com>
2025-08-16 07:27:38 -0500

    Update Fig9_7.R

--- R/Fig9_7.R
+++ R/Fig9_7.R
@@ -35,6 +35,8 @@ Fig9_7 = function ()
   tree=my_rcoal2(n=36,100,100,0)
   depth=get_tree_depth(tree)
   plot(c(0,37,0,37),c(depth,0,depth,0),type='n',axes=F,xlab='',ylab='')
+  quartz()
   plot_tree(tree,muts=T)
 }
+
 
